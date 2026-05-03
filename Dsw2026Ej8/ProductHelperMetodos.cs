85a6a66a472d600560806dbf72134232b48e4190
Ramiro Brito <ramirobritoo21@outlook.com>
2026-05-03 01:36:17 -0300

    Se crea el metodo CalcularPromedio del EJ4

--- Dsw2026Ej8/ProductHelperMetodos.cs
+++ Dsw2026Ej8/ProductHelperMetodos.cs
@@ -35,5 +35,37 @@ namespace Dsw2026Ej8
             return $"{originalValue}-{copia}-{product.GetDescripcion()}";
 
         }
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+
+            int contador = 0;
+            double suma = 0;
+
+            if (nota1.HasValue && nota1.Value >= 0 && nota1.Value <= 10)
+            {
+                suma = nota1.Value;
+                contador++;
+            }
+            if (nota2.HasValue && nota2.Value >= 0 && nota2.Value <= 10)
+            {
+                suma = nota2.Value;
+                contador++;
+            }
+            if (nota3.HasValue && nota3.Value >= 0 && nota3.Value <= 10)
+            {
+                suma = nota3.Value;
+                contador++;
+            }
+            if (contador != 0)
+            {
+                return suma / contador;
+            }
+            else
+            {
+                return 0;
+            }
+
+
+        }
     }
 }
